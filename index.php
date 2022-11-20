e97239492eed6090dfb254eed63ebe763e3549a3
walen6793 <118681585+walen6793@users.noreply.github.com>
2022-11-21 02:02:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,9 +35,7 @@ bootstrap.min.css" >
  <h1 id="temperature">25 °C</h1> 
  </div> 
  <div class="col-md-9 text-center"> 
- <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://
-thingspeak.com/channels/840845/charts/2?
-bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe> 
+ <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1945621/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe> 
  </div> 
  </div> 
  </div> 
